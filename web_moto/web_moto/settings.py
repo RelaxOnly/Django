8763c9fccf545b291f65754c5bca64047e18b567
motoko912 <m10502112@gapps.ntust.edu.tw>
2017-11-19 17:10:28 +0800

    Update timezone and define database

--- web_moto/web_moto/settings.py
+++ web_moto/web_moto/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'x!y!-zrd)s_7=zyhyq_qyup@*3d0b-_b4cly8u+g4j4^evwq1&'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['10.0.2.15']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'mainsite',
 ]
 
 MIDDLEWARE = [
@@ -103,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'en-US'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Taipei'
 
 USE_I18N = True
 
